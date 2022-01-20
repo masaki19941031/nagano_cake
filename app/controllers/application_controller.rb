82536986033d67aaafa7bdde7964b168770ad22e
EC2 Default User <ec2-user@ip-172-31-37-45.ec2.internal>
2022-01-20 09:01:36 +0000

    bootstrap追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  endend
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number])
   end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,55 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+
   devise_for :customers
   devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  devise_for :customers, controllers: {
+  sessions:      'customers/sessions',
+  passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+ }
+
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+ }
+
+  get '/about',to: 'homes#about'
+  get '/search', to: 'searches#search'
+
+  namespace :admins do
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:index, :show, :update]
+    resources :order_details, only: [:update]
+  end
+
+  #customer
+
+    resource :customer, only: [:show, :edit, :update] do
+      collection do
+        get 'unsubscribe'
+        patch 'withdraw'
+      end
+    end
+
+    resources :items, only: [:index, :show]
+
+    resources :cart_items, only: [:index, :update, :create, :destroy] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+
+    resources :orders, only: [:index, :show, :new, :create] do
+      collection do
+        post 'confirm'
+        get 'complete'
+      end
+    end
+
+    resources :addresses, except: [:show, :new]
+
+    end
